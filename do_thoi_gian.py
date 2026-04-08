17e2c56ca16ea637a63606279c5d0299951a4877
Will20061505 <datdeptrai.2006515@gmail.com>
2026-04-08 16:50:05 +0700

    push

---
+++ do_thoi_gian.py
@@ -0,0 +1,10 @@
+import time
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"{func.__name__} chạy trong {end - start:.6f} giây")
+        return result
+    return wrapper
